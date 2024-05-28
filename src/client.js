aae61b127b0954fc8d966cf02ed4fe0e02921ae7
Toha Ikhsan <ikhsanwalker27@gmail.com>
2024-05-29 00:38:09 +0700

    asu

--- src/client.js
+++ src/client.js
@@ -8,6 +8,11 @@ const port = process.env.PORT || 5000;
 // app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join('./src/public')));
 
-app.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
+// app.listen(port, () => {
+//   console.log(`Server berjalan di http://localhost:${port}`);
+// });
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('./src/public/index.html'));
 });
+
+export default app;

4ea3bb882163d4d27964775c2010f4ab8897dd23
Toha Ikhsan <ikhsanwalker27@gmail.com>
2024-08-08 20:08:17 +0700

    deleted comment

--- src/client.js
+++ src/client.js
@@ -5,7 +5,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-// app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join('./src/public')));
 
 app.listen(port, () => {
